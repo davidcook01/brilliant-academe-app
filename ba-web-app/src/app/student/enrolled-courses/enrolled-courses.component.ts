93f6c19552b0e8a761b6dc1c402cbcf038cd9c6e
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2019-03-19 15:52:43 -0400

    Successfully mapped course API call object to dynamic enrolled-course component cards

--- ba-web-app/src/app/student/enrolled-courses/enrolled-courses.component.ts
+++ ba-web-app/src/app/student/enrolled-courses/enrolled-courses.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
+
+interface Course {
+  courseDescription: string;
+  courseId: string;
+  courseName: string;
+  coverImage: string;
+  instructorId: string;
+  instructorName: string;
+  percentageCompleted: number;
+}
 
 @Component({
   selector: 'app-enrolled-courses',
@@ -7,9 +18,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnrolledCoursesComponent implements OnInit {
 
-  constructor() { }
+  public courses: Course[];
+
+  constructor() {
+    this.courses = [];
+  }
+
+  public async getCourses() {
+    try {
+      return await axios.get('https://cors-anywhere.herokuapp.com/https://dxumyaeyh4.execute-api.us-east-1.amazonaws.com/ba-api/user/afff130b-0e99-4234-9b2d-db85224c1280/enrollment')
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  public async showCourses() {
+    const courses = await this.getCourses();
+    if (courses) {
+      this.courses.push(courses.data.courses); 
+      console.log(this.courses[0][0].coverImage);
+    }
+  }
 
   ngOnInit() {
+    this.showCourses();
   }
 
 }
