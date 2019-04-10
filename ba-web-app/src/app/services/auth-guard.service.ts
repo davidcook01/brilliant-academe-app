3011a2dffe8aaf9ca9ae51275cd568867ae5f49b
DevTeam <rajinder.soni@brilliantinfotech.com>
2019-04-10 12:51:09 -0400

    Corrected code formatting, routes changed for end of session routing

--- ba-web-app/src/app/services/auth-guard.service.ts
+++ ba-web-app/src/app/services/auth-guard.service.ts
@@ -7,22 +7,22 @@ import { AuthService } from './auth-service';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthGuardService {
+
   constructor(
     private router: Router,
     private auth: AuthService
   ) { }
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> {
-      return this.auth.isAuthenticated()
-        .pipe(
-          tap(loggedIn => {
-            if (!loggedIn) {
-              this.router.navigate(['/login']);
-            }
-          })
-        );
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.auth.isAuthenticated()
+      .pipe(
+        tap(loggedIn => {
+          if (!loggedIn) {
+            this.router.navigate(['/']);
+          }
+        })
+      );
   }
 }
